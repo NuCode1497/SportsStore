5ce8f19b219b9985752fe363857bfe322a98841a
Cody <cody@smartxadvisory.com>
2019-01-25 14:41:20 -0500

    Changed name of Color to FurColor

--- SportsStore/Models/ViewModels/Dog.cs
+++ SportsStore/Models/ViewModels/Dog.cs
@@ -8,6 +8,6 @@ namespace SportsStore.Models.ViewModels
     public class Dog
     {
         public string Name { get; set; }
-        public string Color { get; set; }
+        public string FurColor { get; set; }
     }
 }
