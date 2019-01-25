fc2426997452a01404bcc6da2dee4473ac4cc3d5
Cody <cody@smartxadvisory.com>
2019-01-25 14:42:50 -0500

    made a typo

--- SportsStore/Models/ViewModels/PagingInfo.cs
+++ SportsStore/Models/ViewModels/PagingInfo.cs
@@ -6,7 +6,7 @@ namespace SportsStore.Models.ViewModels
     {
         public int TotalItems { get; set; }
         public int ItemsPerPage { get; set; }
-        public int CurrentPage { get; set; }
+        public int CurredfgdfgdfgntPage { get; set; }
 
         public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
     }
